5ec50c6dc643681f664ba12466ca6717c6500407
Sigmanificient <edhyjox@gmail.com>
2021-07-24 11:55:49 +0200

    rewriting unittests

--- test/test_lexer.py
+++ test/test_lexer.py
@@ -1,17 +1,30 @@
 import json
 from klivi.lexer import Lexer
 
+import unittest
 
-def test_answer():
-    html_content = ""
-    json_content = ""
 
-    with open("mock_data/sample.html", "r") as html_file:
-        html_content = html_file.read().replace("\n", "")
-        html_file.close()
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
 
-    with open("mock_data/sample.json", "r") as json_file:
-        json_content = json_file.read().replace("\n", "")
-        json_file.close()
+    def test_answer(self):
+        html_content = ""
+        json_content = ""
 
-    assert json.loads(Lexer(html_content).lexify()) == json.loads(json_content)
+        with open("mock_data/sample.html", "r") as html_file:
+            html_content = html_file.read().replace("\n", "")
+            html_file.close()
+
+        with open("mock_data/sample.json", "r") as json_file:
+            json_content = json_file.read().replace("\n", "")
+            json_file.close()
+
+        self.assertEqual(
+            json.loads(Lexer(html_content).lexify()),
+            json.loads(json_content)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test/test_queue.py
+++ test/test_queue.py
@@ -1,14 +1,21 @@
 from klivi.queue import Queue
 
-
-def test_enqueue():
-    queue = Queue()
-    queue.enqueue(1)
-    assert queue.queue == [1]
-    assert queue.enqueue(1) == True  # it return True
+import unittest
 
 
-def test_dequeue():
-    queue = Queue()
-    queue.enqueue(1)
-    assert queue.dequeue() == 1
+class MyTestCase(unittest.TestCase):
+    def test_enqueue(self):
+        queue = Queue()
+        queue.enqueue(1)
+
+        self.assertEqual(queue.queue, [1])
+        self.assertTrue(queue.enqueue(1))
+
+    def test_dequeue(self):
+        queue = Queue()
+        queue.enqueue(1)
+        self.assertEqual(queue.dequeue(), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
